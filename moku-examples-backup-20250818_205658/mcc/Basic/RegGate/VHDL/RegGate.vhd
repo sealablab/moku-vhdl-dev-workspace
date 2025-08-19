102dd7f34570b5ab717b4f10c5ee0bff027fbf22
johnny cache <johnycsh@DRP-e1.local>
2025-08-18 21:07:52 -0700

    Remove accidentally committed backup directory
    - Remove moku-examples-backup-20250818_205658/ (was accidentally committed)
    - Clean up repository - backup directories should not be tracked
    - Keep workspace focused and clean
    - Backup was only meant as temporary safety measure during submodule setup

--- moku-examples-backup-20250818_205658/mcc/Basic/RegGate/VHDL/RegGate.vhd
+++
@@ -1,5 +0,0 @@
-architecture Behavioural of CustomWrapper is
-begin
-    OutputA <= InputA when Control1(0) = '1' else (others => '0');
-    OutputB <= InputB when Control1(1) = '1' else (others => '0');
-end architecture;
